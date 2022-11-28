777365d799a4c0ff9dcd5c39aede3ba689e01400
Tatiana Osipova <otv120780@gmail.com>
2022-11-28 13:22:35 -0500

    feat: style arrows

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(function(){
 
-    $('photo__slider').click();
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="arrow-right"></button>',
+
+    });
 
 });
